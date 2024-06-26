65c42f7110c6615886c6f62c40157db5c4b121c3
Xavier Bigwood <91576107+rxsOwO@users.noreply.github.com>
2024-06-25 21:24:57 -0400

    Update roll20.js
    addded initiaitve supprot tracker poooyp

--- roll20.js
+++ roll20.js
@@ -4,7 +4,12 @@ btn = chat.getElementsByTagName("button")[0];
 
 
 function postChatMessage(msg){
+  if(msg.type != "Initiative") {
     txt.value = "&{template:sf_generic} {{name="+msg.playername +"}} {{characterid=" + msg.playername + "}} {{title="+ msg.type +"}} {{r1name=CHECK}}  {{r1=[[" + msg.roll + "]]}}";
+  }
+  else {
+    txt.value = "&{template:sf_generic} {{name="+msg.playername +"}} {{characterid=" + msg.playername + "}} {{title="+ msg.type +"}} {{r1name=CHECK}}  {{r1=[[" + msg.roll + "&{tracker}]]}}";
+  }  
     btn.click();
 }
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
